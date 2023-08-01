00b04d9bb8ed0c09a274258cba47172baefb19a5
ilya-asta <133906800+ilya-asta@users.noreply.github.com>
2023-08-01 18:00:30 +0300

    Add files via upload

--- HomeWorkS4.py
+++ HomeWorkS4.py
@@ -43,18 +43,6 @@ from random import randint
 # 9'''
 # n = 5 # кустов
 
-# list_1 = list(randint(1, 5) for i in range(int(input('Введите кол-во кустов: '))))
-# print(list_1)
-# a = int(input('Введите № куста: '))
-# res = 0
-# if a == 1:
-#     res = list_1[0] + list_1[1] + list_1[-1]
-# elif a == len(list_1):
-#     res = list_1[-2] + list_1[-1] + list_1[0]
-# else:
-#     res = list_1[a-1] + list_1[a-2] + list_1[a]
-# print(res, 'ягод')
-
 
 
 
